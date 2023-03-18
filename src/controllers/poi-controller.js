139aae3946455a2b851e76ab219f474d31272fbb
Greg <6regmcc@gmail.com>
2023-03-18 00:03:44 +0000

    finished map

--- src/controllers/poi-controller.js
+++ src/controllers/poi-controller.js
@@ -19,7 +19,8 @@ export const poiController = {
         name: request.payload.name,
         category: request.payload.category,
         description: request.payload.description,
-        location: request.payload.location,
+        latitude: request.payload.latitude,
+        longitude: request.payload.longitude,
       };
       await db.poiStore.addPoi(newPoi);
       return h.redirect("/dashboard");
