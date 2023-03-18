c60e61b0675f657c069d40ca2e7fc99ba1445f35
Greg <6regmcc@gmail.com>
2023-03-18 21:26:58 +0000

    added poi test

--- test/fixtures.js
+++ test/fixtures.js
@@ -25,3 +25,37 @@ export const testUsers = [
     password: "secret",
   },
 ];
+
+export const tiknock = {
+  name: "Tiknock ",
+  category: "Hike trail head",
+  description: "Ticknock offers up to 10 km of mountain and forest walks with some spectacular views of Dublin City, Dublin Bay, Bray Head and Wicklow Mountains.",
+  latitude: 53.255792,
+  longitude: -6.249579,
+};
+
+export const testPois = [
+  {
+    name: "Hell Fire Club",
+    category: "Monument",
+    description:
+      "A club that was founded in 1735 by Richard Parsons – a man known for dabbling in black magic. Its members met at locations across Dublin and were known for their amoral behaviour and debauchery involving alcohol and sex.",
+    latitude: 53.251709,
+    longitude: -6.32967,
+  },
+  {
+    name: "Ballyedmonduff Wedge Tomb",
+    category: "Monument",
+    description:
+      "This is an early bronze age wedge tomb, circa 1700 BC, and was situated in a small clearing in a planted forest before the trees surrounding it were cut down in 2020.",
+    latitude: 53.229038,
+    longitude: -6.226298,
+  },
+  {
+    name: "Drimnagh Castle",
+    category: "Monument",
+    description: "Drimnagh Castle is a Norman castle located in Drimnagh, a suburb of Dublin, Ireland. It is the only remaining castle in Ireland with a flooded moat around it",
+    latitude: 53.324811,
+    longitude: -6.332846,
+  },
+];
