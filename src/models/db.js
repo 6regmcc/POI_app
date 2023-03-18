5364ec96e923eb2123a1980fef36ae311e7f8519
Greg <6regmcc@gmail.com>
2023-03-18 16:55:01 +0000

    mongo store added

--- src/models/db.js
+++ src/models/db.js
@@ -1,12 +1,23 @@
 import { userMemStore } from "./mem/user-mem-store.js";
 import { poiMemStore } from "./mem/poi-mem-store.js";
+import { connectMongo } from "./mongo/connect.js";
+import { userMongoStore } from "./mongo/user-mongo-store.js";
+import { poiMongoStore } from "./mongo/poi-mongo-store.js";
 
 export const db = {
   userStore: null,
   poiStore: null,
 
-  init() {
-    this.userStore = userMemStore;
-    this.poiStore = poiMemStore;
+  init(storeType) {
+    switch (storeType) {
+      case "mongo":
+        this.userStore = userMongoStore;
+        this.poiStore = poiMongoStore;
+        connectMongo();
+        break;
+      default:
+        this.userStore = userMemStore;
+        this.poiStore = poiMemStore;
+    }
   },
 };
